4d899b1f41db7a18c01e8f18ba407d0724cb3daa
cenelnik@yandex.ru <cenelnik@yandex.ru>
2024-04-08 03:05:26 +0300

    Добавьте файлы проекта.

---
+++ DZ13.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace DZ13
+{
+    /// <summary>
+    /// 13я домашка по OUTUS. Linq
+    /// </summary>
+    class DZ13
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 2, 1, 5, 4, 3, 6, 9, 8, 7};
+            foreach(var i in list.Top(20)) Console.WriteLine($"Next value: {i}");
+
+            Func<Persone, int> Pred = (persone) => persone.Age;
+
+            List<Persone> list2 = new List<Persone>();
+            list2.Add(new Persone {Name = "AAAA", Age = 10 });
+            list2.Add(new Persone { Name = "BBBB", Age = 12 });
+            list2.Add(new Persone { Name = "CCCC", Age = 14 });
+            list2.Add(new Persone { Name = "DDDD", Age = 50 });
+            list2.Add(new Persone { Name = "EEEE", Age = 45 });
+            list2.Add(new Persone { Name = "FFFF", Age = 47 });
+            list2.Add(new Persone { Name = "JJJJ", Age = 96 });
+
+            
+            list2.Top(50, person => person.Age);
+
+            foreach (var i in list2.Top(50, person => person.Age)) Console.WriteLine($"Next value: {i.Name}:{i.Age}");
+        }
+
+
+        public class Persone 
+        {
+            public string Name { get; set; } = "";
+            public int Age { get; set; } = 0;
+        }
+
+    }
+}
---
+++ EnumerableExtensionMethods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ13
+{
+    
+    internal static class EnumerableExtensionMethods
+    {
+        /// <summary>
+        /// метод расширения, принимающий значение Х от 1 до 100 и 
+        /// возвращающий заданное количество процентов от выборки с 
+        /// округлением количества элементов в большую сторону.
+        /// </summary>
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int percent)
+        {
+            if (!(percent < 100 && percent >= 0)) throw new ArgumentException($"Value must be more than 0 and less than 100. {percent} is uncorrect.");
+            
+            foreach (var item in source)
+            {
+                if (!(item is int) || (item is null)) throw new ArgumentException($"Use integer array only.");
+            }
+
+            int IntegerPart = source.Count() * percent / 100;
+            double FullNumber = source.Count() * percent / 100.00;
+            if ((FullNumber - IntegerPart) * 100 > 50) IntegerPart += 1;
+
+            return (IEnumerable<T>)source.OrderByDescending(n => Convert.ToInt32(n)).Take(IntegerPart);
+        }
+
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int percent, Func<T, int> predicate)
+        {
+            if (!(percent < 100 && percent >= 0)) throw new ArgumentException($"Value must be more than 0 and less than 100. {percent} is uncorrect.");
+
+            int IntegerPart = source.Count() * percent / 100;
+            double FullNumber = source.Count() * percent / 100.00;
+            if ((FullNumber - IntegerPart) * 100 > 50) IntegerPart += 1;
+
+            return (IEnumerable<T>)source.OrderByDescending(n => predicate(n)).Take(IntegerPart);
+        }
+    }
+}
